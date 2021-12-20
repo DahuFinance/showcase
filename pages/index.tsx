d827c0c41e981c6e627410116d5828cec2d3ddf6
Leo <leohub@live.fr>
2021-12-20 15:30:26 +0100

    Add social network meta's

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,10 +15,26 @@ export default function Home() {
             className="flex flex-col min-h-screen bg-gradient-to-br from-darkBlue to-purple lg:from-darkBlue lg:via-purpledarky lg:to-darkBlue">
             <Head>
                 <title>Dahu Finance | Home</title>
+
+                {/* Primary */}
                 <meta name="description" content="Dahu Finance 🐐 is an all-in-one solution to provide you innovative services around decentralized finance on $AVAX "/>
                 <meta name="keywords" content="DahuFinance, Dahu, Defi, AMM, DEX, Avax, Avalanche, Swap, Farm, Stake, Compound"/>
                 <meta name="author" content="DahuFinance"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+                {/* Open Graph / Facebook */}
+                <meta property="og:type" content="website"/>
+                <meta property="og:url" content="https://dahu.finance/"/>
+                <meta property="og:title" content="Dahu Finance | Home"/>
+                <meta property="og:description" content="Dahu Finance 🐐 is an all-in-one solution to provide you innovative services around decentralized finance on $AVAX "/>
+                <meta property="og:image" content=""/>
+
+                {/* Twitter */}
+                <meta property="twitter:card" content="summary_large_image"/>
+                <meta property="twitter:url" content="https://dahu.finance/"/>
+                <meta property="twitter:title" content="Dahu Finance | Home"/>
+                <meta property="twitter:description" content="Dahu Finance 🐐 is an all-in-one solution to provide you innovative services around decentralized finance on $AVAX "/>
+                <meta property="twitter:image" content=""/>
             </Head>
             <Nav/>
             <main>
