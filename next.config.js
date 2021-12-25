fe521afd5ffb3115458ee5a458c8fe80cd32a2da
Leo <leohub@live.fr>
2021-12-25 13:59:18 +0100

    Revert www

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
     trailingSlash: true,
     images: {
         loader: 'imgix',
-        path: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://www.dahu.finance'
+        path: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://dahu.finance'
     }
 }
